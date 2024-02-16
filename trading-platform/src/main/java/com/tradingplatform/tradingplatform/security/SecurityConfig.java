f944da3f345ff2a385eb36c38e1b5ca1bada3af3
Bartlomiej Mikler <bartlomiej.mikler@motorolasolutions.com>
2024-02-16 15:18:48 +0100

    authentication principal in TradeController

--- trading-platform/src/main/java/com/tradingplatform/tradingplatform/security/SecurityConfig.java
+++ trading-platform/src/main/java/com/tradingplatform/tradingplatform/security/SecurityConfig.java
@@ -1,20 +1,15 @@
 package com.tradingplatform.tradingplatform.security;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.HttpStatusEntryPoint;
-import org.springframework.security.web.authentication.www.BasicAuthenticationEntryPoint;
-import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
 
 @Configuration
 class SecurityConfig {
--- trading-platform/src/main/java/com/tradingplatform/tradingplatform/trade/TradeController.java
+++ trading-platform/src/main/java/com/tradingplatform/tradingplatform/trade/TradeController.java
@@ -3,7 +3,6 @@ package com.tradingplatform.tradingplatform.trade;
 import com.tradingplatform.tradingplatform.rate.CryptoCurrency;
 import com.tradingplatform.tradingplatform.user.SecurityUser;
 import jakarta.validation.constraints.DecimalMin;
-import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -19,26 +18,20 @@ class TradeController {
 
     private final TradeService tradeService;
 
-    @GetMapping
-    String test(@AuthenticationPrincipal SecurityUser principal) {
-        System.err.println(principal.getId() + " " + principal.getUsername() + " " + principal.getAuthorities());
-        return "TradeController works";
-    }
-
     @PostMapping("/offer")
-    ResponseEntity<TradeOffer> getOffer(@RequestParam UUID userId, @RequestBody TradeRequest tradeRequest) {
-        TradeOffer tradeOffer = tradeService.createOffer(new TradeOfferCommand(userId, tradeRequest.currency(), tradeRequest.amount()));
+    ResponseEntity<TradeOffer> getOffer(@AuthenticationPrincipal SecurityUser user, @RequestBody TradeRequest tradeRequest) {
+        TradeOffer tradeOffer = tradeService.createOffer(new TradeOfferCommand(user.getId(), tradeRequest.currency(), tradeRequest.amount()));
         return ResponseEntity.ok(tradeOffer);
     }
 
     @PostMapping("/buy")
-    void buy(@RequestParam UUID userId, @RequestBody UUID tradeOfferId) {
-        tradeService.buy(new TradeCommand(userId, tradeOfferId));
+    void buy(@AuthenticationPrincipal SecurityUser user, @RequestBody UUID tradeOfferId) {
+        tradeService.buy(new TradeCommand(user.getId(), tradeOfferId));
     }
 
     @PostMapping("/sell")
-    void sell(@RequestParam UUID userId, @RequestBody UUID tradeOfferId) {
-        tradeService.sell(new TradeCommand(userId, tradeOfferId));
+    void sell(@AuthenticationPrincipal SecurityUser user, @RequestBody UUID tradeOfferId) {
+        tradeService.sell(new TradeCommand(user.getId(), tradeOfferId));
     }
 }
 
