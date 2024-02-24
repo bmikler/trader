fa8312ad10bab9094db5e6d07522387ac13f3c00
bmikler <bartlomiej.mikler@gmail.com>
2024-02-24 18:37:39 +0100

    integration test project structure first try

---
+++ trading-platform/src/integration-test/java/tradingplatform/IntegrationTest.java
@@ -0,0 +1,23 @@
+package tradingplatform;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class IntegrationTest {
+
+
+    @Test
+    void getAccountInfo() {
+        System.err.println("abc");
+    }
+
+}
---
+++ trading-platform/src/integration-test/java/tradingplatform/trade/AccountControllerTest.java
@@ -0,0 +1,13 @@
+package tradingplatform.trade;
+
+import org.junit.jupiter.api.Test;
+import tradingplatform.IntegrationTest;
+
+class AccountControllerTest extends IntegrationTest {
+
+    @Test
+    void getAccountInfo() {
+        System.err.println("abc");
+    }
+
+}
