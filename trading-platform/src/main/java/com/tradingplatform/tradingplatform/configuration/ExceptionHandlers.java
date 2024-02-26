b348cb383206e8c0c171add73d616f6fef313c98
Bartlomiej Mikler <bartlomiej.mikler@motorolasolutions.com>
2024-02-26 17:24:36 +0100

    basic exception handler

---
+++ trading-platform/src/main/java/com/tradingplatform/tradingplatform/configuration/ExceptionHandlers.java
@@ -0,0 +1,36 @@
+package com.tradingplatform.tradingplatform.configuration;
+
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.List;
+
+@ControllerAdvice
+class ExceptionHandlers extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    ResponseEntity<GlobalError> handleEntityNotFoundException(EntityNotFoundException ex) {
+        return new ResponseEntity<>(new GlobalError(ex.getMessage()), HttpStatus.NOT_FOUND);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+        return new ResponseEntity<>(new ValidationErrors(
+                ex.getFieldErrors().stream()
+                        .map(fieldError -> new ValidationError(fieldError.getField(), fieldError.getDefaultMessage()))
+                        .toList()
+        ), HttpStatus.BAD_REQUEST);
+    }
+
+    record GlobalError(String errorMessage) { }
+    record ValidationErrors(List<ValidationError> errors) { }
+    record ValidationError(String fieldName, String message) { }
+}
