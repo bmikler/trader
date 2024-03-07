37649ec901c2e97c3e20db190927bc08868d8849
Bartlomiej Mikler <bartlomiej.mikler@motorolasolutions.com>
2024-03-07 14:55:53 +0100

    api gateway fixed

--- api-gateway/src/main/java/com/trader/apigateway/RouteConfig.java
+++ api-gateway/src/main/java/com/trader/apigateway/RouteConfig.java
@@ -10,12 +10,13 @@ class RouteConfig {
     @Bean
     RouteLocator myRoutes(RouteLocatorBuilder builder) {
         return builder.routes()
-                .route("path_route", r -> r.path("/**")
+                .route("exchange-service", r -> r.path("/exchange/**")
+                        .filters(f -> f.stripPrefix(1))
                         .uri("lb://EXCHANGE"))
-                .route("trading-platform", r -> r.path("/**")
+                .route("trade-service", r -> r.path("/trade/**")
+                        .filters(f -> f.stripPrefix(1))
                         .uri("lb://TRADING-PLATFORM"))
                 .build();
     }
-
 }
 
